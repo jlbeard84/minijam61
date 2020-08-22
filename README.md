6c7792e89a196ea0f4182be70ff49376b1a31724
Josh Beard <josh@jlbeard.com>
2020-08-21 22:26:59 -0400

    Initial framework commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # minijam61
 Game for Mini Jam 61 (Egyptian Theme)
+
+
+### Libraries used
+- [Classic](https://github.com/rxi/classic)
---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    Object = require "libs.classic"
+end
+
+function love.keypressed(key)
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
